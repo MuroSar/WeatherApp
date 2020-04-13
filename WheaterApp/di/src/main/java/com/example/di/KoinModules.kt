8f5791e5a27d7a4669c03913289d0254a6e5dac1
Braian Quinteros <braian.quinteros@globant.com>
2020-04-13 14:06:06 -0300

    changed some issues

--- WheaterApp/di/src/main/java/com/example/di/KoinModules.kt
+++ WheaterApp/di/src/main/java/com/example/di/KoinModules.kt
@@ -1,12 +1,12 @@
 package com.example.di
 
-import com.example.domain.usecases.implementation.CreateCityListUseCaseImpl
-import com.example.domain.usecases.GetCityByIdUseCase
-import com.example.domain.usecases.implementation.GetCityByIdUseCaseImpl
 import com.example.domain.usecases.CreateCityListUseCase
+import com.example.domain.usecases.GetCityByIdUseCase
+import com.example.domain.usecases.implementation.CreateCityListUseCaseImpl
+import com.example.domain.usecases.implementation.GetCityByIdUseCaseImpl
 import org.koin.dsl.module
 
 val useCasesModule = module {
-    single<GetCityByIdUseCase> { GetCityByIdUseCaseImpl() }
-    single<CreateCityListUseCase> { CreateCityListUseCaseImpl() }
+    factory<GetCityByIdUseCase> { GetCityByIdUseCaseImpl() }
+    factory<CreateCityListUseCase> { CreateCityListUseCaseImpl() }
 }
